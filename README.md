bd53cbc3c937037235a2b4a2984c43f0bf0eb7f3
Todd Alloway <kinome79@yahoo.com>
2023-02-11 22:57:56 -0500

    Initial Commit - Project submit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anonymous Message Board
+
+This is the boilerplate for the Anonymous Message Board project. Instructions for completing your project can be found at https://www.freecodecamp.org/learn/information-security/information-security-projects/anonymous-message-board
+
+## Completed Project
+
+Project was completed by kinome79 ©2023 to finish the assignment. 
---
+++ routes/api.js
@@ -0,0 +1,315 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+const {ObjectId} = require('mongodb');
+
+// Reply Schema - object for storing thread replies as a subdocument of Thread models
+const ReplySchema = new Schema({
+  text: {type: String, required: true},
+  created_on: {type: Date, default: Date.now, required: true},
+  delete_password: {type: String, required: true},
+  reported: {type: Boolean, default: false}
+})
+
+// Thread Schema - object for storing threads as a document in mongoose database
+const ThreadSchema = new Schema({
+  board: {type: String, required: true},
+  text: {type: String, required: true},
+  created_on: {type: Date, default: Date.now, required: true},
+  bumped_on: {type: Date, default: Date.now, required: true},
+  delete_password: {type: String, required: true},
+  reported: {type: Boolean, default: false},
+  replies: [ReplySchema]
+})
+
+// Create Thread and Reply models for creating database objects
+const Thread = mongoose.model("Thread", ThreadSchema);
+const Reply = mongoose.model("Reply", ReplySchema)
+
+// Connect Mongoose Database - report when connected
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+  console.log('Mongo Database Connected!!!');
+}).catch(err => {
+  console.log('Failed to connect to Mongo Database!!!')
+});
+
+module.exports = function (app) {
+  
+  // create routes for manipulating threads
+  app.route('/api/threads/:board')
+
+  // thread get request - returns with json object containing at max 10 threads, and 3 replies each, hiding password and reported fields
+  .get( (req,res) => {
+    
+    // get board name from URL
+    const board = req.params.board;
+    let results = [];
+
+    // request database to reply with at least 10 boards, sorted by bumped on date, minus reported and pw fields
+    Thread.find({board: board}).sort("-bumped_on").limit(10)
+                            .select("-reported -delete_password -__v -replies.reported -replies.delete_password").exec((err, data) => {
+
+      if (err) { return res.send(err) }  // report errors if any
+
+      // Go through resulting threads and limit replies to three
+      results = data.map( thread => {
+        thread.replies = thread.replies.slice(-3);
+        return thread;
+      });
+
+      // Send json response with data
+      return res.json(results);
+    });
+  })
+
+  // thread post request - creates a new thread when supplied with text and password - response with the thread
+  .post( (req,res) => {
+    
+    // get data from URL and the body of the request
+    const board = req.params.board;
+    const {text, delete_password} = req.body;
+
+    // if data is present, create the thread with hashed password
+    if (text && delete_password) {
+      Thread.create({board: board, text: text, delete_password: bcrypt.hashSync(delete_password, 4)}, (err, obj) => {
+        if (err) { return res.send(err)}
+
+        // on success send json response with thread object
+        return res.json(obj);
+      });
+
+    // if not all data was supplied, reply with missing required fields  
+    } else {
+      return res.send("missing required fields");
+    }
+  })
+
+  // Thread put request - reports a thread, responds with the string reported on success
+  .put( (req,res) => {
+    
+    // get data from URL and the body of the request
+    const board = req.params.board;
+    let thread_id = req.body.thread_id;
+
+    // if thread ID supplied, perform find and update on that thread ID to set reported to true
+    if (thread_id) {
+
+      // verify thread_id is a valid database object ID, respond with string 'invalid id' otherwise
+      try {thread_id = ObjectId(thread_id);} catch(e) {return res.send("invalid id")} 
+
+      // perform find and update on desired thread
+      Thread.findOneAndUpdate({_id: thread_id, board:board}, {reported: true}, (err,obj) => {
+        if (err) { return res.send(err) }  // report any errors
+        if (obj) { return res.send("reported") }   // if object is returned, respond with string 'success'
+        else { return res.send("invalid board/id") }   // if no object returned, respond with string 'invalid board/id'
+      });
+
+    // if no valid thread_id supplied, reply with 'missing required fields'  
+    } else {
+      return res.send("missing required field");
+    }
+  })
+
+  // thread delete request - delete thread with supplied id and password- responds with success on completion
+  .delete( (req,res) => {
+    
+    // get data from URL and the body of the request
+    const board = req.params.board;
+    let {thread_id, delete_password} = req.body;
+
+    // if id and password supplied, perform database search to find the desired thread
+    if (thread_id && delete_password) {
+
+      // verify thread_id is a valid database object ID, respond with string 'invalid id' otherwise
+      try {thread_id = ObjectId(thread_id);} catch(e) {return res.send("invalid id")}
+
+      Thread.findOne({_id: thread_id, board: board}, (err, thread) => {
+        if (err) { return res.send(err) }   // print any errors that occur
+        if (!thread) { return res.send("invalid board/id") }   // if no thread returned, respond with 'invalid board/id'
+
+        // if retrieved threads password matches the supplied password, delete thread
+        if (bcrypt.compareSync(delete_password, thread.delete_password)) {
+          thread.delete( err => {
+            if (err) { return res.send(err) }
+            else { return res.send("success") }  // if no error, respond with string 'success'
+          });
+
+        // if password doesn't match, respond with string 'incorrect password'  
+        } else {
+          return res.send("incorrect password");
+        }
+      });
+    
+    // if id or password wasn't supplied, respond with 'missing required fields'  
+    } else {
+      return res.send("missing required fields");
+    }
+  });
+    
+  // create routes for manipulating replies
+  app.route('/api/replies/:board')
+
+  // reply get request - retried a single thread with all replies - responds with the thread with replies
+  .get( (req,res) => {
+    
+    // get data from url and query variables
+    const board = req.params.board;
+    let thread_id = req.query.thread_id;
+    
+    // if thread id was supplied, find the thread
+    if (thread_id) {
+
+      // verify thread_id is a valid database object ID, respond with string 'invalid id' otherwise
+      try {thread_id = ObjectId(thread_id);} catch(e) {return res.send("invalid id")}
+
+      // perform database search, returning results without reported or password fields
+      Thread.findOne({board:board, _id:thread_id}).select("-reported -delete_password -__v -replies.reported -replies.delete_password").exec( (err,thread) => {
+        if (err) { return res.send(err) }    // if any errors, send response with the error
+        if (!thread) { return res.send("invalid board/id")}    // if no thread returned, send response stating 'invalid board/id'
+
+        // If valid thread found, send json response with that thread
+        return res.json(thread); 
+      });
+
+    // if thread id wasn't supplied, response with string 'missing required fields'  
+    } else {
+      return res.send("missing required fields");
+    }
+  })
+
+  // reply post request - create new reply given thread id, text and password - response with reply json object and updates bumped date
+  .post( (req,res) => {
+    
+    // get data from url and request body
+    const board = req.params.board; 
+    let {text, delete_password, thread_id} = req.body;
+
+    // if all required data supplied, retrieve desired thread, and append new reply
+    if (text && delete_password && thread_id) {
+
+      // verify thread_id is a valid database object ID, respond with string 'invalid id' otherwise
+      try {thread_id = ObjectId(thread_id);} catch(e) {return res.send("invalid id")}
+
+      // retreive thread from database with board and thread id
+      Thread.findOne({board:board, _id: thread_id}, (err, thread) => {
+        if (err) { return res.send(err) }   // if error, response with error text
+        if (!thread) { return res.send("invalid board/id")}   // if no thread returned, respond with 'invalid board/id'
+
+        // push new reply to the thread with data and hashed password, update bumped on date, and save thread
+        thread.replies.push(new Reply({text: text, delete_password: bcrypt.hashSync(delete_password, 4)}));
+        thread.bumped_on = Date.now();
+        thread.save(err => {
+          if (err) { return res.send(err) }
+
+          // after save completes, send json response with the reply object
+          return res.json(thread.replies[thread.replies.length -1]);
+        });
+      });
+
+    // if not all data supplied, send response 'missing required fields'  
+    } else {
+      return res.send("missing required fields")
+    }
+
+  })
+
+  // reply put requests - updates the reported field on reply to true - response with text 'reported'
+  .put( (req,res) => {
+    
+    // get data from urls and request body
+    const board = req.params.board;
+    let {thread_id, reply_id} = req.body;
+
+    // if thread and reply ids are supplied, proceed with database lookup
+    if (thread_id && reply_id) {
+
+      // verify thread and reply IDs are valid database object ids, if not respond 'invalid id'
+      try {
+        thread_id = ObjectId(thread_id);
+        reply_id = ObjectId(reply_id);
+      } catch(e) {return res.send("invalid id")}
+
+      // retrieve desired thread object from database
+      Thread.findOne({_id: thread_id, board:board}, (err,thread) => {
+        if (err) { return res.send(err) }   // on error send error response
+        if (!thread) { return res.send("invalid board/id") }   // if no thread found respond 'invalid board/id'
+
+        // retrieve desired reply object
+        let reply = thread.replies.id(reply_id);
+
+        // if reply found, change reported to true and save database
+        if (reply) {
+          reply.reported = true;
+          thread.save( (err) => {
+            if (err) { return res.send(err)}
+
+            // send string response 'reported'
+            return res.send("reported");
+          });
+
+        // if reply not found, respond 'invalid id'  
+        } else {
+          return res.send("invalid id")
+        }
+      });
+
+    // if either id wasn't supplied, respond with 'missing required field'  
+    } else {
+      return res.send("missing required field");
+    }
+  })
+
+  // reply delete request - marks reply as deleted - responds with success
+  .delete( (req,res) => {
+    
+    // get data from URL and request body
+    const board = req.params.board;
+    let {thread_id, reply_id, delete_password} = req.body;
+
+    // if required data is supplied, proceed with retrieving object
+    if (thread_id && delete_password && reply_id) {
+
+      // verify id's are valid object ids, if not respond 'invalid id'
+      try {
+        thread_id = ObjectId(thread_id);
+        reply_id = ObjectId(reply_id);
+      } catch(e) {return res.send("invalid id")}
+
+      // retrieve desired thread from database with board and thread id
+      Thread.findOne({_id: thread_id, board: board}, (err, thread) => {
+        if (err) { return res.send(err) }   // if error, respond with error
+        if (!thread) { return res.send("invalid board/id")}   // if thread not found, respond 'invalid board/id'
+
+        // retrieve desired reply from thread
+        let reply = thread.replies.id(reply_id);
+
+        // if reply found, verify the password, and set reply's text to '[deleted]' and save
+        if (reply) {
+          if (bcrypt.compareSync(delete_password, reply.delete_password)) {
+            reply.text = "[deleted]";
+            thread.save( (err) => {
+              if (err) { return res.send(err) }
+
+              // send text response with 'success'
+              return res.send("success") 
+            })
+
+          // if password is invalid, respond with 'incorrect password'  
+          } else {
+            return res.send("incorrect password");
+          }
+
+        // if thread not found, respond with 'invalid id'  
+        } else {
+          return res.send("invalid id");
+        }
+      });
+
+    // if not all fields supplied, reply 'missing required fields'  
+    } else {
+      return res.send("missing required fields");
+    }
+  });
+};
